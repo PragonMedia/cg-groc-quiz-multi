47cd72c0faedd674afbce95dbf8835c2e5099a1d
19miguel19lim <tech@paragonmedia.io>
2026-09-01 22:23:57 +0800

    Strip clickid/rtkcid on Over 75 Medicare No Claim Now redirect

--- js/app.js
+++ js/app.js
@@ -500,12 +500,22 @@ var CLAIM_CLOAK_FAIL_URL = "https://www.google.com";
 
 function buildClaimNowHref() {
   var url = buildUrlWithCurrentParams();
-  var clickID =
-    url.searchParams.get("clickid") ||
-    localStorage.getItem("rt_clickid") ||
-    "";
-  if (clickID) {
-    url.searchParams.set("clickid", clickID);
+
+  // Over 75 + Medicare No (disqualified): keep all params except clickid/rtkcid
+  if (
+    url.searchParams.get("qualified") === "no" &&
+    url.searchParams.get("age") === "80"
+  ) {
+    url.searchParams.delete("clickid");
+    url.searchParams.delete("rtkcid");
+  } else {
+    var clickID =
+      url.searchParams.get("clickid") ||
+      localStorage.getItem("rt_clickid") ||
+      "";
+    if (clickID) {
+      url.searchParams.set("clickid", clickID);
+    }
   }
 
   var target = new URL(CLAIM_REDIRECT_HREF, window.location.href);
